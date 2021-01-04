6c113740ab8537dc8379854c48d29a25923d5706
Paweł <paprzytula@gmail.com>
2021-01-04 16:42:42 +0100

    Add project files.

---
+++ Company-API/Services/LoggerService.cs
@@ -0,0 +1,29 @@
+﻿using Company_API.Contracts;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Company_API.Services
+{
+    public class LoggerService : ILoggerService
+    {
+        public static ILogger logger = LogManager.GetCurrentClassLogger();
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);        }
+
+        public void LogError(string message)
+        {
+            logger.Error(message);        }
+
+        public void LogInfo(string message)
+        {
+            logger.Info(message);        }
+
+        public void LogWarn(string message)
+        {
+            logger.Warn(message);        }
+    }
+}
---
+++ Company-API/Startup.cs
@@ -0,0 +1,100 @@
+using Company_API.Contracts;
+using Company_API.Data;
+using Company_API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Company_API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddCors(o => {
+                o.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    );
+            });
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("0.0.1", new OpenApiInfo {
+                    Title="Company API",
+                    Version = "0.0.1",
+                    Description= "This is API for Company. Versioning: https://semver.org/lang/pl/"
+                });
+                var xfile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xpath = Path.Combine(AppContext.BaseDirectory, xfile);
+                c.IncludeXmlComments(xpath);
+            });
+
+            services.AddSingleton<ILoggerService, LoggerService>();
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseMigrationsEndPoint();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c => {
+                c.SwaggerEndpoint("/swagger/0.0.1/swagger.json", "Company API");
+                c.RoutePrefix = "";
+            });
+
+
+            app.UseHttpsRedirection();
+            app.UseCors("CorsPolicy");
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
